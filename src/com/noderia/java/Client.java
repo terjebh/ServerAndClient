a31e276ecd0eaa89b5241c8b0fb121ebf944b78b
Terje Berg-Hansen <terje@itfakultetet.no>
2021-08-03 16:27:35 +0200

    Initial commit

---
+++ src/com/noderia/java/Client.java
@@ -0,0 +1,48 @@
+package com.noderia.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+
+
+
+    public static void main(String[] args) {
+
+        final String IP = args[0];
+        final int PORT = Integer.parseInt(args[1]);
+
+        System.out.println("IP: "+IP);
+        System.out.println("Port: "+PORT);
+
+        try {
+            Socket socket = new Socket(IP,PORT);
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+
+            while(true) {
+                System.out.print("> ");
+                String command = keyboard.readLine();
+                if(command.equals("quit")) break;
+                out.println(command);
+                String serverResponse = input.readLine();
+                System.out.println("Server says: "+serverResponse);
+            }
+
+            socket.close();
+            System.exit(0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+}
---
+++ src/com/noderia/java/Server.java
@@ -0,0 +1,54 @@
+package com.noderia.java;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class Server {
+
+    private static final int PORT = 9898;
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket listener = new ServerSocket(PORT);
+        System.out.println("[SERVER] Server running, waiting for connections... ");
+        Socket client = listener.accept();
+        System.out.println("Client connected");
+        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        try {
+            while(true) {
+                String request = in.readLine().toLowerCase(Locale.ROOT);
+
+                if(request.contains("date")) {
+                    out.println("The date today is: " + LocalDate.now());
+                } else if (request.contains("lagre")) {
+                    FileWriter notatFil = new FileWriter("notater.txt",true);
+                    notatFil.append(request.replace("lagre","").trim()+"\n");
+                    notatFil.close();
+                    out.println("Notatet er lagret");
+                    System.out.println("Notat lagret...");
+                } else if (request.contains("vis")) {
+                    StringBuffer notater = new StringBuffer();
+                    notater.append("Innholdet i filen notater.txt\n\n");
+                    Files.lines(Paths.get("notater.txt")).forEach(a -> notater.append(a+"\n"));
+                    out.println(notater.toString());
+                } else {
+                   out.println("I don't know what that means...");
+                }
+            }
+        } finally {
+            in.close();
+            out.close();
+            out.println("Goodbye...");
+            System.exit(0);
+        }
+
+    }
+
+}
